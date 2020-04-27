bba1afd8e5e8fa294c36169608b1cb23cc077390
KiraSamHughes <60794613+KiraSamHughes@users.noreply.github.com>
2020-04-27 15:50:04 -0400

    First Commit

---
+++ Assets/Scripts/Colors.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Colors : MonoBehaviour
+{
+ 
+    public Button Green;
+    public Button Red;
+    public Button Blue;
+    public Button Shadow; 
+
+    public int isSkinRedSold;
+    public int isSkinBlueSold;
+    public int isSkinShadowSold;
+    public int SkinColor;
+
+    public SpriteRenderer sprite;
+
+    void Start()
+    {
+       
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //SkinColor= playerprefs.getint("SkinColor"); put spriterenderer sprite at top
+        //if (SkinColor== 1)
+        //{ sprite.color = new Color(1, 0, 0, 1);}
+        isSkinRedSold = PlayerPrefs.GetInt("isSkinRedSold");
+        isSkinBlueSold = PlayerPrefs.GetInt("isSkinBlueSold");
+        isSkinShadowSold = PlayerPrefs.GetInt("isSkinShadowSold");
+
+        if (isSkinRedSold == 1)
+        {
+            Red.interactable = true;
+        }
+        else
+        {
+            Red.interactable = false;
+        }
+        if (isSkinBlueSold == 1)
+        {
+            Blue.interactable = true;
+        }
+        else
+        {
+            Blue.interactable = false;
+        }
+
+        if (isSkinShadowSold == 1)
+        {
+            Shadow.interactable = true;
+        }
+        else
+        {
+            Shadow.interactable = false;
+        }
+        // Change the 'color' property of the 'Sprite Renderer'
+
+    }
+    public void TurnRed()
+    {
+        sprite.color = new Color(1, 0, 0, 1);
+        PlayerPrefs.SetInt("SkinColor", 1);
+    }
+    public void TurnBlue()
+    {
+        sprite.color = new Color(0, 0, 1, 1);
+        PlayerPrefs.SetInt("SkinColor", 2);
+    }
+
+    public void TurnGreen()
+    {
+        sprite.color = new Color(255, 255, 255, 1);
+        PlayerPrefs.SetInt("SkinColor", 0);
+    }
+
+    public void TurnShadow()
+    {
+        sprite.color = new Color(0, 0, 0, 1);
+        PlayerPrefs.SetInt("SkinColor", 3);
+    }
+
+}
---
+++ Assets/Scripts/ShopControlScript.cs
@@ -0,0 +1,94 @@
+﻿//
+//ShopControlScript.cs for unity-chan!
+//
+//Original Script is here:
+//ricopin / ShopControlScript.cs
+//Rocket Jump : http://rocketjump.skr.jp/unity3d/109/
+//https://twitter.com/ricopin416
+//
+//Revised by N.Kobayashi 2014/06/20
+//Anima2D adaptation by Mandarina Games 2016/03/18
+//
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopControlScript : MonoBehaviour
+{
+    public int coinCount;
+    public int isSkinRedSold;
+    public int isSkinBlueSold;
+    public int isSkinShadowSold;
+
+   // public Text coinText;
+    public Text skinPrice1;
+    public Text skinPrice2;
+    public Text skinPrice3;
+
+    public Button buyRedButton;
+    public Button buyBlueButton;
+    public Button buyShadowButton;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        coinCount = PlayerPrefs.GetInt("coinCount");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //coinText.text = "Fossils: " + coinCount;
+
+        isSkinRedSold = PlayerPrefs.GetInt("isSkinRedSold");
+        isSkinBlueSold = PlayerPrefs.GetInt("isSkinBlueSold");
+        isSkinShadowSold = PlayerPrefs.GetInt("isSkinShadowSold");
+
+        if (coinCount >= 20 && isSkinRedSold == 0)
+            buyRedButton.interactable = true;
+        else
+            buyRedButton.interactable = false;
+
+        if (coinCount >= 20 && isSkinBlueSold == 0)
+            buyBlueButton.interactable = true;
+        else
+            buyBlueButton.interactable = false;
+
+        if (coinCount >= 50 && isSkinShadowSold == 0)
+            buyShadowButton.interactable = true;
+        else
+            buyShadowButton.interactable = false;
+    }
+
+    public void BuyRedSkin()
+    {
+        coinCount -= 20;
+        PlayerPrefs.SetInt("isSkinRedSold", 1);
+        skinPrice1.text = "Sold!";
+        //  buyRedButton.gameObject.SetActive(false);
+    }
+
+    public void BuyBlueSkin()
+    {
+        coinCount -= 20;
+        PlayerPrefs.SetInt("isSkinBlueSold", 1);
+        skinPrice2.text = "Sold!";
+       // buyBlueButton.gameObject.SetActive(false);
+    }
+
+    public void BuyShadowSkin()
+    {
+        coinCount -= 50;
+        PlayerPrefs.SetInt("isSkinShadowSold", 1);
+        skinPrice3.text = "Sold!";
+      //  buyShadowButton.gameObject.SetActive(false);
+    }
+
+    public void ResetShop()
+    {
+        PlayerPrefs.SetInt("isSkinRedSold", 0);
+        PlayerPrefs.SetInt("isSkinBlueSold", 0);
+        PlayerPrefs.SetInt("isSkinShadowSold", 0);
+    }
+}
+
